85a98d69b260993036d466f6a8ce8154b55f7528
Andres6936 <andres6936@gmail.com>
2026-01-13 19:27:36 -0500

    Add Tamagui Babel plugin and update project configuration
    Integrated `@tamagui/babel-plugin` for improved build-time optimizations and added its configuration in `babel.config.js`. Updated `app-scanner/package.json` to support clean builds and reflect dependency changes in `bun.lock`.

--- app-scanner/babel.config.js
+++ app-scanner/babel.config.js
@@ -2,6 +2,20 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: [["inline-import", {"extensions": [".sql"]}], ["react-native-worklets/plugin"]] // <-- add this
+        plugins: [
+            [
+                "inline-import",
+                {"extensions": [".sql"]}
+            ],
+            ["react-native-worklets/plugin"],
+            [
+                '@tamagui/babel-plugin',
+                {
+                    components: ['tamagui'],
+                    config: './tamagui.config.ts',
+                    logTimings: true,
+                    disableExtraction: process.env.NODE_ENV === 'development',
+                },],
+        ]
     };
 };
